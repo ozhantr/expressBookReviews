4f513f247b43d2a0a17a2501ea07984735228209
Oezhan Duran <ozhantr@hotmail.com>
2025-03-17 18:14:44 +0100

    Completed Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,9 +29,21 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  let author = req.params.author;
+  let matchingBooks = [];
+
+  Object.keys(books).forEach((key) => {
+    if (books[key].author === author) {
+      matchingBooks.push(books[key]);
+    }
+  });
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({ message: "No books found for the given author" });
+  }
 });
 
 // Get all books based on title
