09c410157c5aed0ba88fb23d2b287d71cac6c105
Oezhan Duran <ozhantr@hotmail.com>
2025-03-17 18:08:27 +0100

    Completed Task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,16 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  let isbn = req.params.isbn;
+  let book = books[isbn];
+
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
